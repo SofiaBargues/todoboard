d2e5497c7948c7f19ce631e833866d7472b70c60
Sofia Bargues <barguessofia@gmail.com>
2024-01-16 16:40:23 +0000

    una

---
+++ src/components/Input.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Input = () => {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Add a task" />
+        <button>Add</button>
+      </form>
+    </>
+  );
+};
