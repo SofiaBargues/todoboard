e06112aa5916290ca6a3addc221b52f9f0bf7bec
Sofia Bargues <barguessofia@gmail.com>
2024-01-17 05:18:54 +0000

    algo

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -20,7 +20,7 @@ const Board = ({
   return (
     <>
       <div
-        className="max-w-lg rounded-xl flex flex-col items-center justify-start 
+        className="max-w-lg rounded-xl flex flex-col items-center justify-between 
        text-center border text-lg pt-3 pb-4 px-4"
       >
         <p>{task}</p>
